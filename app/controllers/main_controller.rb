2f3ec6633b91dbb2257f3a47350155d4a8950bfe
Paul Kenrick <pkenrick@gmail.com>
2019-03-21 22:50:42 +0000

    Updated #callback action to submit correct params. Updated controller spec to be more specific.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,22 @@ class MainController < ApplicationController
   end
 
   def callback
-    uri = URI('http://mic-leads.dev-test.makeiteasy.com/api/v1/create')
-    params = {}
-    res = Net::HTTP.post_form(uri, params)
-    puts "============ #{res.body}"
+    uri = URI("#{ENV['LEAD_API_URI']}/api/v1/create")
+    res = Net::HTTP.post_form(uri, callback_params)
+  end
+
+  private
+
+  def callback_params
+    {
+      access_token: ENV['SECRET_KEY_BASE'],
+      pGUID: ENV['LEAD_API_PGUID'],
+      pAccName: ENV['LEAD_API_PACCNAME'],
+      pPartner: ENV['LEAD_API_PPARTNER'],
+      name: params[:name],
+      business_name: params[:business_name],
+      email: params[:email],
+      telephone_number: params[:telephone_number]
+    }
   end
 end
--- spec/controller/main_controller_spec.rb
+++ spec/controller/main_controller_spec.rb
@@ -14,9 +14,14 @@ RSpec.describe MainController, type: :controller do
       stub_request(:post, "http://mic-leads.dev-test.makeiteasy.com/api/v1/create").to_return(status: 200, body: "This is a stubbed response", headers: {})
     end
 
-    it 'makes a request to the test api' do
-      expect(Net::HTTP).to receive(:post_form).with(URI("http://mic-leads.dev-test.makeiteasy.com/api/v1/create"), {}).and_call_original
-      post :callback
+    it 'makes a request to the test api with correct params' do
+      uri = URI("http://mic-leads.dev-test.makeiteasy.com/api/v1/create")
+      expect(Net::HTTP).to receive(:post_form)
+        .with(uri, hash_including(name: "Test Name", business_name: "Test Business Name", email: "test@example.com", telephone_number: "01234567891"))
+        .and_call_original
+
+      post :callback, params: {name: "Test Name", business_name: "Test Business Name", email: "test@example.com", telephone_number: "01234567891"}
+
     end
   end
 
